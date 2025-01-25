49924c77dbcc2a93386fa1ede33f0b607088c142
GreenFrog <rstarck.biz@gmail.com>
2025-01-25 23:09:38 +0000

    Improvements

--- Network.py
+++ Network.py
@@ -1,59 +1,56 @@
 import numpy as np
 from tqdm import tqdm
 from time import sleep
+import matplotlib
 class Network:
-    def __init__(self, data):
+    def __init__(self, data, passes, learnRate):
         self.data = data
-        self.learnRate = 0.0000000001
-        self.passes = 50
-        self.coef = np.random.randn(len(self.data))
-        self.allCoef = []
-        self.allCoef.append(self.coef)
-        self.loss = []
-        self.totalLoss = 1000000
-        print('')
+        self.dataX = np.zeros(len(self.data))
+        self.dataY = np.zeros(len(self.data))
+        self.coef = np.random.randint(-300,300,len(data))
+        self.passes = passes
+        for i in range(len(self.data)):
+            self.dataX.put(i, self.data[i][0])
+            self.dataY.put(i, self.data[i][1])
+        self.passes = passes
+        self.learnRate = learnRate
+
+        self.graph = []
 
         
         
         
         
     def train(self):
-        for loops in range(self.passes):
-            self.grad = []
-            self.calcY = 0
-            self.delatY = []
-            for i in range(0,len(self.data)):
-                self.grad.append(0)
-                self.delatY.append(0)
-            self.totalLoss = 0
+        self.losses = []
+        self.deltaLoss = 5
+        self.prevLoss = 0
+        while self.deltaLoss > 0.000001 and self.deltaLoss < 10000:
+            self.grad = np.zeros(len(self.data))
+            self.delatY = np.zeros(len(self.data))
+            self.dataYHat = np.zeros(len(self.data))
             self.forwardPass()
             self.backPass()
-            #print(self.totalLoss)
             self.tweak()
-            self.allCoef.append(self.coef)
-            #print('(',self.allCoef[loops][0],',',self.allCoef[loops][1],',',self.loss[loops],')')
-            #print(self.coef)
-            #print(self.totalLoss)
-        #print(self.totalLoss)
+            self.deltaLoss = self.totalLoss - self.prevLoss
+            self.prevLoss = self.totalLoss
+        self.MinLoss = self.totalLoss
     
     def forwardPass(self):
-        for i in range(0,len(self.data)):
-            self.calcY = 0
-            for j in range(0,len(self.data)):
-                self.calcY += self.coef[j] * (self.data[i][0]**j)
-            self.delatY[i] = (self.calcY - self.data[i][1])
-            self.totalLoss += (self.calcY - self.data[i][1])**2
-            self.loss.append(self.totalLoss)
+        for i in range(len(self.dataX)):
+            self.yHat = np.dot(self.coef, self.dataX[i])
+            self.dataYHat.put(i, self.yHat)
+        self.delatY = self.dataYHat- self.dataY
+        self.totalLoss = np.sum(self.delatY**2)
+        
             
     
     def backPass(self):
-        for i in range(0,len(self.data)):
-            for j in range(0,len(self.data)):
-                self.grad[j] += 2*self.delatY[i]*(self.data[i][0]**j)
+        self.grad = 2*self.delatY * self.dataX
     
     def tweak(self):
-        for i in range(0,len(self.coef)):
-            self.coef[i] = self.coef[i] - self.learnRate * self.grad[i]
+        mag = np.linalg.norm(self.grad)
+        self.coef = self.coef - self.learnRate * np.sqrt(mag)*self.grad
     
 
 
--- getPoints.py
+++ getPoints.py
@@ -1,8 +1,16 @@
 import turtle 
 import Network
 import numpy
+from tqdm import tqdm
 
-NoPoints = 4
+
+import numpy as np
+
+NoPoints = 6
+losses = []
+deltaLosses = []
+coef = []
+learningRates= []
 def drawAxis():
   turtle.forward(1000)
   turtle.backward(2000)
@@ -12,30 +20,41 @@ def drawAxis():
   turtle.backward(2000)
   turtle.forward(1000)
   turtle.right(90)
-  
+
 def plotGraph(coefficients):
   turtle.pencolor("red")
-  for i in range(-610,610):
+  for i in tqdm(range(-610,610)):
     yCoord = 0
     for j in range(len(coefficients)-1,-1,-1):
       yCoord += coefficients[j]*(i**j)
     turtle.goto(i,yCoord)
     turtle.pendown()
-    
-
 
 
 def train():
-    coefficients = numpy.zeros(NoPoints)
-    bob = Network.Network(points)
-    bob.train()
-    coefficients = bob.coef
-    print(coefficients)
-    plotGraph(coefficients)
-    print('Done!')
+    for i in tqdm(range(-18,0)):
+      for j in tqdm(range(0,1000000)):
+        bob = Network.Network(points, 50, 1*(10**(-(i))))
+        bob.train()
+        losses.append(bob.MinLoss)
+        coef.append(bob.coef)
+        deltaLosses.append(bob.deltaLoss)
+        learningRates.append(bob.learnRate)
+    print(min(losses))
+    print(coef[losses.index(min(losses))])
+    print(deltaLosses[losses.index(min(losses))])
+    print(learningRates[losses.index(min(losses))])
+    count = 0
+    for i in tqdm(range(0,len(deltaLosses)-1)):
+      if deltaLosses[i] < 1:
+        count += 1
+    print("Global minimums found = ",count,"/",len(deltaLosses))
+    plotGraph(coef[losses.index(min(losses))])
+
+
 # screen object 
 wn = turtle.Screen() 
-  
+
 # method to perform action 
 def fxn(x, y): 
   points.append([x, y])
@@ -48,8 +67,7 @@ def fxn(x, y):
 # onclick action
 points = []
 turtle.speed(0)
-turtle.screensize(canvwidth=400, canvheight=400, 
-                  bg="white")
+
 drawAxis()
 turtle.penup()
 turtle.shape("circle") 
